510c0c159285946bfcc59b0b7e8d921ccc53cda9
Povilas Balciunas <balciunas90@gmail.com>
2014-10-14 13:03:33 +0000

    Updates installation script to setup ssl support.

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ main() {
 # 1. Changes config directory password.
 # 2. Changes database domain to dc=povilasb,dc=com.
 # 3. Creates entries for sample unix users.
+# 4. Enables SSL support.
 #
 install() {
 	admin_password="admin"
@@ -40,6 +41,8 @@ install() {
 		-f data/setup_unix_users.ldif
 	ldapadd -x -D "cn=admin,cn=config" -w ${admin_password} \
 		-f data/change_log_level.ldif
+
+	setup_ssl
 }
 
 
@@ -52,6 +55,23 @@ setup_ssl() {
 		openssl req -newkey rsa:1024 -x509 -nodes \
 			-out ${ssl_key_file} -keyout ${ssl_key_file} -days 3650
 	fi
+
+	ldap_unix_user=openldap
+	ldap_unix_group=openldap
+	chown -R ${ldap_unix_user}:${ldap_unix_group} ${ssl_key_dir}
+
+	modify_slapd_defaults
+
+	ldapadd -x -D "cn=admin,cn=config" -w ${admin_password} \
+		-f data/setup_ssl.ldif
+	service slapd restart
+}
+
+
+modify_slapd_defaults() {
+	slapd_services="ldap\:\/\/\/ ldaps\:\/\/\/ ldapi\:\/\/\/"
+	sed -i "s/^SLAPD_SERVICES.*/SLAPD_SERVICES=\"${slapd_services}\"/g" \
+		/etc/default/slapd
 }
 
 
